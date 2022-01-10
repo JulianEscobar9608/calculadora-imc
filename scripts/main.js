219b00cd61f41c8f372fbd4f906ca3dba0a740aa
JulianEscobar9608 <julian96081@gmail.com>
2022-01-10 00:04:28 -0500

    correccion

--- scripts/main.js
+++ scripts/main.js
@@ -104,21 +104,21 @@ function local(genero,imc){
         captura[0] += 1;
         localStorage.setItem(nombreJson,JSON.stringify(captura)); 
 
-    }else if (imc>=18.5&&imc<=24.9){
+    }else if (imc>=18.5&&imc<=24.999){
         elemento.querySelector('h3').setAttribute('class','verde');
         let captura = JSON.parse(localStorage.getItem(nombreJson));
         captura[1] += 1;
         localStorage.setItem(nombreJson,JSON.stringify(captura)); 
 
 
-    }else if(imc>=25.0&&imc<=29.9){
+    }else if(imc>=25.0&&imc<=29.999){
         elemento.querySelector('h3').setAttribute('class','amarillo');
         let captura = JSON.parse(localStorage.getItem(nombreJson));
         captura[2] += 1;
         localStorage.setItem(nombreJson,JSON.stringify(captura)); 
 
 
-    }else if(imc>=30.0&&imc<=39.9){
+    }else if(imc>=30.0&&imc<=39.999){
         elemento.querySelector('h3').setAttribute('class','rojo');
         let captura = JSON.parse(localStorage.getItem(nombreJson));
         captura[3] += 1;
